9c1585e4fe85f36e3d5138f3d83751e54f38245a
didibelarusmy <33047171-didibelarusmy@users.noreply.replit.com>
2024-03-25 15:39:08 +0000

    Initial commit

---
+++ HashTableFixed.js
@@ -0,0 +1,76 @@
+class HashTable {
+  constructor(size) {
+    this.table = new Array(size);
+    this.size = size;
+  }
+
+  hash(key) {
+    let total = 0;
+    for (let i = 0; i < key.length; i++) {
+      total += key.charCodeAt(i);
+    }
+    console.log('index', total % this.size)
+    return total % this.size;
+  }
+
+  set(key, value) {
+    const index = this.hash(key);
+    // this.table[index] = value;
+    const bucket = this.table[index];
+    if (!bucket) {
+      this.table[index] = [[key, value]];
+    } else {
+      const sameKeyItem = bucket.find((item) => item[0] === key);
+      if (sameKeyItem) {
+        sameKeyItem[1] = value;
+      } else {
+        bucket.push([key, value]);
+      }
+    }
+  }
+
+  get(key) {
+    const index = this.hash(key);
+    // return this.table[index];
+    const bucket = this.table[index];
+    if (bucket) {
+      const sameKeyItem = bucket.find((item) => item[0] === key);
+      if (sameKeyItem) {
+        return sameKeyItem[1];
+      }
+    }
+    return undefined;
+  }
+
+  remove(key) {
+    let index = this.hash(key);
+    // this.table[index] = undefined;
+    const bucket = this.table[index];
+    if (bucket) {
+      const sameKeyItem = bucket.find((item) => item[0] === key);
+      if (sameKeyItem) {
+        bucket.splice(bucket.indexOf(sameKeyItem), 1);
+      }
+    }
+  }
+
+  display() {
+    for (let i = 0; i < this.table.length; i++) {
+      if (this.table[i]) {
+        console.log('display', i, this.table[i][0], this.table[i][1]);
+      }
+    }
+  }
+}
+
+const table = new HashTable(50);
+table.set("name", "Diana");
+table.set("age", 33);
+table.display();
+console.log(table.get("name"));
+
+table.set("mane", "Thijs");
+table.set("name", "Jan");
+table.remove("name");
+
+table.display();
